62c6a5091a00c4af9e619ff0b28a3f82ede907ff
js2601 <js2601@iolani.org>
2025-03-19 12:14:13 -1000

    unfinished balance handling

--- backend/data.js
+++ backend/data.js
@@ -1,6 +1,6 @@
 var fs = require('fs')
 var data = {}
 
-data.push({user: 1, balance: 3})
+data.push({username: "donovan", balance: 1000})
 let json = JSON.stringify(data)
 fs.writeFile('data.json', json, 'utf8', callback)
