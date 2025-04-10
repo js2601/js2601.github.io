9ca6194515402c42d2db73980fbd6bd6b0d7476a
js2601 <js2601@iolani.org>
2025-04-09 14:45:53 -1000

    access control

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+const allowedIPs = ['185.199.108.153', '185.199.109.153', '185.199.110.153', 
+  '185.199.111.153'];
 
 const usersRoute = require('./routes/user.js')
 const setbalRoute = require('./routes/setbal.js')
 const addUserRoute = require('./routes/adduser.js')
+
+app.use((req, res, next) => {
+  const ip = req.ip;
+
+  if (allowedIPs.includes(ip)) {
+    next();
+  } else {
+    res.status(403).send('Forbidden')
+  }
+})
 
 app.use('/', usersRoute)
 app.use('/', setbalRoute)
